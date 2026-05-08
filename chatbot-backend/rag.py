deacf0742748955ea636e6b7fbde4b4a452889c2
Bose <bosem3708@gmail.com>
2026-05-08 17:49:12 +0530

    Properly ignore chroma_db and sqlite, update requirements and READMEs

--- chatbot-backend/rag.py
+++ chatbot-backend/rag.py
@@ -2,7 +2,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
 embedding = HuggingFaceEmbeddings(
-    model_name="all-MiniLM-L6-v2"
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
 )
 
 vectordb = Chroma(
